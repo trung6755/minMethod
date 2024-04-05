a55c0ed4510d77ea3614e07ad2b99d9b3ffd7697
trung6755 <trungggrr@gmail.com>
2024-04-05 09:28:39 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace minMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 4, 3, 6, 2, 6, 8, 3, 9, 1 };
+            int index = Min(arr);
+            Console.WriteLine("The min value is: " + arr[index]);
+        }
+
+        public static int Min(int[] arr)
+        {
+            int min = arr[0];
+            int index = 0;
+
+            for(int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] < min)
+                {
+                    min = arr[i];
+                    index = i;
+                }
+            }
+
+            return index;
+        }
+    }
+}
